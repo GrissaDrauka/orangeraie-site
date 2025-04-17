69b6218e52c60cb941a8a40ccf85d2579b4e6378
Rémi <wattier.remi@gmail.com>
2025-04-17 22:31:18 +0200

    FooterComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   template: `
   
   <!--<div class="min-h-screen bg-[#E6D9BC]">-->
@@ -17,53 +18,8 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
         📞 Réserver
       </a>
     </router-outlet>
-
-    <!-- Footer -->
-    <!--<div class="h-6 bg-gradient-to-b from-transparent to-[#f9f4ef]"></div>-->
-    <footer class="bg-[#f9f4ef] text-gray-700 pt-12 pb-10 px-4 md:px-16">
-      <div class="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-sm">
-        
-        <!-- Nom / logo -->
-        <div>
-          <h3 class="text-xl font-serif font-bold text-orangeraie mb-3">L'Orangeraie</h3>
-          <p class="text-gray-600">Chambres d’hôtes & Gîte à Elne</p>
-        </div>
-
-        <!-- Navigation -->
-        <div class="space-y-2">
-          <h4 class="font-semibold text-orangeraie">Navigation</h4>
-          <ul class="space-y-1">
-            <li><a routerLink="/" class="hover:underline cursor-pointer">Accueil</a></li>
-            <li><a routerLink="/chambres" class="hover:underline cursor-pointer">Nos chambres</a></li>
-            <li><a routerLink="/gite" class="hover:underline cursor-pointer">Notre gîte</a></li>
-            <li><a routerLink="/contact" class="hover:underline cursor-pointer">Contact</a></li>
-          </ul>
-        </div>
-
-        <!-- Contact -->
-        <div class="space-y-2">
-          <h4 class="font-semibold text-orangeraie">Contact</h4>
-          <p>
-        📍 <a href="https://www.google.com/maps?q=29+Avenue+Paul+Reig,+66200+Elne"
-              target="_blank" rel="noopener noreferrer"
-              class="hover:underline">
-                29 Avenue Paul Reig, 66200 Elne
-            </a>
-          </p>
-          <p>📞 <a href="tel:+33468822279" class="hover:underline">+33(0)4 68 82 22 79</a></p>
-          <p>📧 <a href="mailto:contact@orangeraie-elne.fr" class="hover:underline">contact&#64;orangeraie-elne.fr</a></p>
-        </div>
-      </div>
-
-      <!-- Mentions -->
-      <div class="text-center text-xs text-gray-500 mt-10">
-        &copy; {{ currentYear }} L'Orangeraie — Tous droits réservés.
-      </div>
-    </footer>
-
-<!--</div> -->
+    <app-footer></app-footer>
   `
 })
 export class AppComponent {
-  currentYear: number = new Date().getFullYear();
 }
