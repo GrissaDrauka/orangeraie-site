2209c3441afe2d61be5257b3a7ed704536561ddd
Rémi <wattier.remi@gmail.com>
2025-04-18 16:21:46 +0200

    Amelioration page chambres

--- src/app/pages/chambres/chambres.component.ts
+++ src/app/pages/chambres/chambres.component.ts
@@ -11,19 +11,22 @@ export class ChambresComponent {
       nom: 'Chambre Marine',
       description: 'Laissez-vous bercer par une atmosphère marine, entre bleu doux et esprit bord de mer.',
       image: 'assets/chambres/chambre_marine.jpg',
-      nbPers: '2 pers.'
+      nbPers: '2 pers.',
+      tarif: '49€/nuit'
     },
     {
       nom: 'Chambre Nature',
       description: 'Une chambre habillée de fer forgé aux motifs végétaux pour une touche de nature élégante.',
       image: 'assets/chambres/chambre_nature.jpg',
-      nbPers: '2 pers.'
+      nbPers: '2 pers.',
+      tarif: '49€/nuit'
     },
     {
       nom: 'Chambre Bambouseraie',
       description: 'Une chambre apaisante à l’ambiance zen, inspirée par la douceur des bambous.',
       image: 'assets/chambres/chambre_bambouseraie.jpg',
-      nbPers: '2 pers.'
+      nbPers: '2 pers.',
+      tarif: '49€/nuit'
     }
   ];
 
@@ -32,13 +35,15 @@ export class ChambresComponent {
       nom: 'Chambre l\'Atelier',
       description: 'Une chambre au caractère affirmé, entre bois, métal et esprit atelier, pour un séjour inspiré.',
       image: 'assets/chambres/chambre_atelier.jpg',
-      nbPers: '3 pers.'
+      nbPers: '3 pers.',
+      tarif: '62€/nuit'
     },
     {
       nom: 'Chambre Coloniale',
       description: 'Une chambre élégante aux accents d’ailleurs, bois foncé, matières naturelles et charme colonial.',
       image: 'assets/chambres/chambre_coloniale.jpg',
-      nbPers: '4 pers.'
+      nbPers: '4 pers.',
+      tarif: '66€/nuit'
     }
   ];
 }
