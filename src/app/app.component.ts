1b0b9b03bc4e3f7f28052006644dbbc6550bdadf
Rémi <wattier.remi@gmail.com>
2025-04-23 16:49:33 +0200

    Footer sticky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: false,
   template: `
+  <div class="min-h-screen flex flex-col">
     <app-navbar></app-navbar>
 
-    <router-outlet>
-      <!-- Bouton flottant pour mobile -->
-      <a href="tel:+33468822279"
-        class="md:hidden fixed bottom-6 right-6 bg-orangeraie text-white text-sm font-semibold px-4 py-3 rounded-full shadow-lg hover:bg-[#7a583f] transition z-50">
-        📞 Réserver
-      </a>
-    </router-outlet>
+    <main class="flex-grow">
+      <router-outlet>
+        <!-- Bouton flottant pour mobile -->
+        <a href="tel:+33468822279"
+          class="md:hidden fixed bottom-6 right-6 bg-orangeraie text-white text-sm font-semibold px-4 py-3 rounded-full shadow-lg hover:bg-[#7a583f] transition z-50">
+          📞 Réserver
+        </a>
+      </router-outlet>
+    </main>
 
     <app-footer></app-footer>
+  </div>
   `
 })
 export class AppComponent { }
